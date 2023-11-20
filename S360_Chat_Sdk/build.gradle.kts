99d1b629a4485f212b573a0be69c5804f38976da
Vishal Gupta <vishalgupta@Vishals-MacBook-Pro.local>
2023-11-20 15:29:50 +0530

    changes

--- S360_Chat_Sdk/build.gradle.kts
+++ S360_Chat_Sdk/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.library")
+    id ("maven-publish")
 }
 
 android {
@@ -37,3 +38,4 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
+
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.s360example"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.s360example"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
