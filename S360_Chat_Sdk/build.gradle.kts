1ba8ac31251a5636ec04e8faa3dd38530d4cd624
Vishal Gupta <vishalgupta@Vishals-MacBook-Pro.local>
2023-11-21 12:36:48 +0530

    code cleanup

--- S360_Chat_Sdk/build.gradle.kts
+++ S360_Chat_Sdk/build.gradle.kts
@@ -30,9 +30,8 @@ android {
 
 }
 
-
 java {
-    sourceCompatibility = JavaVersion.VERSION_17            // << --- ADD This
+    sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
 }
 
