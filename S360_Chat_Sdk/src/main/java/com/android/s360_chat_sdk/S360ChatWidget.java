fb3bb1b843e1dab2164d69d7b4709ecbe010e83b
Vishal Gupta <vishalgupta@Vishals-MacBook-Pro.local>
2023-11-22 13:43:31 +0530

    added new changes

--- S360_Chat_Sdk/src/main/java/com/android/s360_chat_sdk/S360ChatWidget.java
+++ S360_Chat_Sdk/src/main/java/com/android/s360_chat_sdk/S360ChatWidget.java
@@ -3,7 +3,9 @@ package com.android.s360_chat_sdk;
 import android.content.Context;
 import android.content.Intent;
 
-public class S360ChatWidget {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class S360ChatWidget extends AppCompatActivity {
 
     public static void init(Context context, String url) {
         Intent intent = new Intent(context, MainActivity.class);
