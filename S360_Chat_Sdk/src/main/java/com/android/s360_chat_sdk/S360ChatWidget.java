e75e42b86d3da35d3d8fd193970e7b910dbc0142
Vishal Gupta <vishalgupta@Vishals-MacBook-Pro.local>
2023-11-22 15:51:07 +0530

    removed unused files

--- S360_Chat_Sdk/src/main/java/com/android/s360_chat_sdk/S360ChatWidget.java
+++ S360_Chat_Sdk/src/main/java/com/android/s360_chat_sdk/S360ChatWidget.java
@@ -2,14 +2,25 @@ package com.android.s360_chat_sdk;
 
 import android.content.Context;
 import android.content.Intent;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class S360ChatWidget  {
+    private WebView webView;
 
-    public static void init(Context context, String url) {
-        Intent intent = new Intent(context, MainActivity.class);
-        intent.putExtra("url", url);
-        context.startActivity(intent);
+    public void init(Context context, String url) {
+        webView = new WebView(context);
+
+        // Set a WebViewClient to handle redirects and other events
+        webView.setWebViewClient(new WebViewClient());
+
+        // Enable JavaScript (optional, depending on your needs)
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        // Load the specified URL
+        webView.loadUrl(url);
+
     }
 }
